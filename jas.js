2c9611017c527abdb9f09b4c87ae3ce06fc34cf0
INDAH-780 <mbahindah780@gmail.com>
2024-01-29 23:44:02 +0100

    all

---
+++ jas.js
@@ -0,0 +1,10 @@
+const myStr = "FirstLine\n\t \\SecondLine\nThirdLine"; 
+console.log(myStr);
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " white " + myNoun  + " in the house " + myVerb  + " too " + myAdverb;
+console.log(wordBlanks);
---
+++ tsuip.js
@@ -0,0 +1,22 @@
+function nextInLine(arr, item) {
+    // Only change code below this line
+    
+    return item;
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+  // Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+//lastNameLength = lastName;
+lastNameLength= 8;
+console.log(lastName.length);
